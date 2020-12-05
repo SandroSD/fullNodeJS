1ec5b57be431c7c69a2b1da310d1e520ed4f1422
Sandro Dezerio <sdezerio@gmail.com>
2020-12-05 01:03:02 -0300

    Seguir desde el video 99

--- Node de cero a experto - Udemy/07 - REST Server/server/server.js
+++ Node de cero a experto - Udemy/07 - REST Server/server/server.js
@@ -1,29 +1,22 @@
 require('./config/config');
+
 const express = require('express'); 
+const mongoose = require('mongoose');
+
 const app = express();
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function (req, res){
-	res.json('aa')
-});
 
-app.post('/usuario', function (req, res){
-	let body = req.body;
-	res.json({body})
-});
-
-app.put('/usuario/:id', function (req, res){
-	let id = req.params.id;
-	res.json({id})
-});
-
-app.delete('/usuario', function (req, res){
-	res.json('aa')
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+    if(err) throw err;
+    console.log("Base de datos conectada.");
 });
 
 app.listen(process.env.PORT, () => {
