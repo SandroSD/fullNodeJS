9cabf7cc3a516d83b09301db77a2494bba7d0f06
Sandro Dezerio <sdezerio@gmail.com>
2020-12-08 11:54:13 -0300

    seguimos desde el video 111

--- Node de cero a experto - Udemy/07 - REST Server/server/routes/usuario.js
+++ Node de cero a experto - Udemy/07 - REST Server/server/routes/usuario.js
@@ -1,12 +1,39 @@
 const express = require('express'); 
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 const app = express();
 
 
-app.get('/usuario', function (req, res){
-	res.json('aa')
+app.get('/usuario', function (req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img') //acá podemos listar el nombre de los campos que queremos devolver
+            .skip(desde)
+            .limit(5)
+            .exec( (err, usuarios) => {
+
+                if(err) {
+                    res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+                
+                Usuario.count({ estado: true }, (err, conteo) => {
+
+                    res.json({
+                        ok: true,
+                        usuarios,
+                        cantidad: conteo
+                    })
+                })
+            })
 });
 
 app.post('/usuario', function (req, res){
@@ -36,9 +63,11 @@ app.post('/usuario', function (req, res){
 
 app.put('/usuario/:id', function (req, res){
     let id = req.params.id;
-    let body = req.body;
+    let body = _.pick(req.body, [
+        'nombre', 'email', 'img', 'role', 'estado'
+    ]); //lista de atributos que si deseo actualizar en el put.
 
-    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
         if(err) {
             return res.status(400).json({
                 ok: false,
@@ -53,8 +82,36 @@ app.put('/usuario/:id', function (req, res){
     });
 });
 
-app.delete('/usuario', function (req, res){
-	res.json('aa')
+app.delete('/usuario/:id', function (req, res){
+    let id = req.params.id;
+    
+    let cambiaEstado = {
+        estado: false
+    }
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if(!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario no encontrado"
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    });
 });
 
 module.exports = app;
--- Node de cero a experto - Udemy/07 - REST Server/server/server.js
+++ Node de cero a experto - Udemy/07 - REST Server/server/server.js
@@ -14,7 +14,7 @@ app.use(require('./routes/usuario'));
 
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
     if(err) throw err;
     console.log("Base de datos conectada.");
 });
